2222914b5d460f7e0607a158948b1b64b73ec5c1
Shashwat Pati <shashwatpati02@gmail.com>
2024-06-25 01:59:05 +0530

    done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navigation from "./pages/Auth/Navigation";
 import { ToastContainer } from "react-toastify";
-``;
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
