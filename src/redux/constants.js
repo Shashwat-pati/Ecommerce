3eee9ce18f34d7081bb5a347e3e75efe41d20ba5
Shashwat Pati <shashwatpati02@gmail.com>
2024-09-11 23:41:47 +0530

    proxy

--- src/redux/constants.js
+++ src/redux/constants.js
@@ -1,15 +1,15 @@
 export const BASE_URL = "https://ecommerce-backend-mauve-six.vercel.app";
 export const USERS_URL =
-    "https://ecommerce-backend-mauve-six.vercel.ap/api/users";
+    "https://ecommerce-backend-mauve-six.vercel.app/api/users";
 export const CATEGORY_URL =
-    "https://ecommerce-backend-mauve-six.vercel.ap/api/category";
+    "https://ecommerce-backend-mauve-six.vercel.app/api/category";
 export const PRODUCT_URL =
-    "https://ecommerce-backend-mauve-six.vercel.ap/api/products";
+    "https://ecommerce-backend-mauve-six.vercel.app/api/products";
 export const UPLOAD_URL =
-    "https://ecommerce-backend-mauve-six.vercel.ap/api/upload";
+    "https://ecommerce-backend-mauve-six.vercel.app/api/upload";
 export const ORDERS_URL =
-    "https://ecommerce-backend-mauve-six.vercel.ap/api/orders";
+    "https://ecommerce-backend-mauve-six.vercel.app/api/orders";
 export const PAYPAL_URL =
-    "https://ecommerce-backend-mauve-six.vercel.ap/api/config/paypal";
+    "https://ecommerce-backend-mauve-six.vercel.app/api/config/paypal";
 
 // https://ecommerce-backend-mauve-six.vercel.app
